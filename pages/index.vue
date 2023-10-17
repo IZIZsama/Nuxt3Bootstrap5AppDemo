022b3883d4100e130f7988e5f003d290754b9488
IZIZsama <146801332+IZIZsama@users.noreply.github.com>
2023-10-17 06:16:47 +0000

    変更しました

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="joke-container">
-      <h1>毎日のジョーク！</h1>
-      <p v-if="loading">読み込み中...</p>
-      <p v-else>{{ joke }}</p>
-      <button @click="getJoke" :disabled="loading">もっと笑いたい！</button>
+      <div class="bubble">
+        <div class="shark-image">
+          <h1>シャークジョーク！</h1>
+          <p v-if="loading">読み込み中...</p>
+          <p v-else>{{ joke }}</p>
+        </div>
+      </div>
+      <button @click="getJoke" :disabled="loading">もっとおもろいのへ！</button>
     </div>
   </template>
   
@@ -31,7 +35,7 @@
         }
       },
     },
-    // コンポーネントがマウントされたときにジョークを取得します。
+   
     mounted() {
       this.getJoke();
     },
@@ -44,14 +48,42 @@
     margin: auto;
     text-align: center;
     padding: 20px;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
     border-radius: 5px;
     margin-top: 50px;
+    position: relative;
+  }
+  
+  .bubble {
+    position: relative;
+    background: #f0f0f0;
+    border-radius: 0.4em;
+    padding: 20px;
+  }
+  
+  .shark-image {
+    background: url('shark-image.png') no-repeat center; /* 画像のパスをプロジェクトの構造に合わせて更新してください */
+    background-size: cover; /* 背景画像がコンテナに適合するようにサイズを調整 */
+    padding: 50px; /* 内側のスペースを調整 */
+    color: white; /* テキストの色を調整 */
+    text-shadow: 0px 0px 4px #000000; /* テキストの視認性を高めるための影 */
+  }
+  
+  .bubble:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-top-color: #f0f0f0;
+    border-bottom: 0;
+    border-left: 0;
+    margin-left: -10px;
+    margin-bottom: -20px;
   }
   
   button {
     margin-top: 20px;
   }
   </style>
-  
