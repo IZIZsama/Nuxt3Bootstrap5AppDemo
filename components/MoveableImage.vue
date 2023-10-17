56210ea29842b3a349df823ab3f75a74bcfcb267
IZIZsama <146801332+IZIZsama@users.noreply.github.com>
2023-10-17 06:15:23 +0000

    変更しました

---
+++ components/MoveableImage.vue
@@ -0,0 +1,63 @@
+<template>
+    <div ref="imageContainer" class="image-container">
+      <img :src="imageSrc" :style="{ top: `${y}px`, left: `${x}px` }" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      imageSrc: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        x: 0,
+        y: 0
+      };
+    },
+    mounted() {
+      window.addEventListener('keydown', this.handleKeydown);
+    },
+    beforeDestroy() {
+      window.removeEventListener('keydown', this.handleKeydown);
+    },
+    methods: {
+      handleKeydown(event) {
+        switch (event.keyCode) {
+          case 37: // 左キー
+            this.x -= 10;
+            break;
+          case 38: // 上キー
+            this.y -= 10;
+            break;
+          case 39: // 右キー
+            this.x += 10;
+            break;
+          case 40: // 下キー
+            this.y += 10;
+            break;
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .image-container {
+    position: relative;
+    width: 300px;
+    height: 300px;
+    overflow: hidden;
+  }
+  
+ 
+  
+  img {
+  position: absolute;
+  transition: top 0.3s ease, left 0.3s ease; /* ここを追加 */
+}
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="container">
-    <h1>Hello World</h1>
-  </div>
-</template>
+    <div class="joke-container">
+      <h1>毎日のジョーク！</h1>
+      <p v-if="loading">読み込み中...</p>
+      <p v-else>{{ joke }}</p>
+      <button @click="getJoke" :disabled="loading">もっと笑いたい！</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'JokeComponent',
+    data() {
+      return {
+        joke: '',
+        loading: false,
+      };
+    },
+    methods: {
+      async getJoke() {
+        this.loading = true;
+        try {
+          const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
+          this.joke = response.data.setup + ' ... ' + response.data.punchline;
+        } catch (error) {
+          this.joke = 'エラーが発生しました。ジョークを取得できません。';
+        } finally {
+          this.loading = false;
+        }
+      },
+    },
+    // コンポーネントがマウントされたときにジョークを取得します。
+    mounted() {
+      this.getJoke();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .joke-container {
+    max-width: 400px;
+    margin: auto;
+    text-align: center;
+    padding: 20px;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    border-radius: 5px;
+    margin-top: 50px;
+  }
+  
+  button {
+    margin-top: 20px;
+  }
+  </style>
+  
