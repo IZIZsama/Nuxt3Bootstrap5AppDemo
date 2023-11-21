951d736a100b1f908aa7388e3c15d0e465f3caae
IZIZsama <146801332+IZIZsama@users.noreply.github.com>
2023-11-21 04:34:16 +0000

    a

---
+++ components/Cards.vue
@@ -0,0 +1,16 @@
+<!-- ROOMのゲーム紹介カード -->
+      <div class="card">
+        <a href="https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=d_278563/?dmmref=Mylibrary&i3_ref=recommend" target="_blank" class="card-link">
+          <div class="card-body">
+            <img src="/FhL3mpVaYAAIS3Z.jpg" alt="ROOM" class="card-image">
+            <h3>作品名[ROOM]</h3>
+            <ul>
+              <li>作者・サークル[SORAREVO]</li>
+            </ul>
+            <NuxtLink to="https://twitter.com/SORAREVO">
+              <li>Xアカウント@SORAREVO</li>
+            </NuxtLink>
+            <p>説明・抜きポイント[純粋な新社会人女性を様々なアングルで嬲り歪ませるのは最高でした。現実じゃできないことができるので少し変わった趣味をお持ちの方は必見です！]</p>
+          </div>
+        </a>
+      </div>
--- pages/recommendation.vue
+++ pages/recommendation.vue
@@ -8,22 +8,7 @@
   
       <h2>おすすめのエロゲまとめ！</h2>
   
-      <!-- ROOMのゲーム紹介カード -->
-      <div class="card">
-        <a href="https://www.dmm.co.jp/dc/doujin/-/detail/=/cid=d_278563/?dmmref=Mylibrary&i3_ref=recommend" target="_blank" class="card-link">
-          <div class="card-body">
-            <img src="/FhL3mpVaYAAIS3Z.jpg" alt="ROOM" class="card-image">
-            <h3>作品名[ROOM]</h3>
-            <ul>
-              <li>作者・サークル[SORAREVO]</li>
-            </ul>
-            <NuxtLink to="https://twitter.com/SORAREVO">
-              <li>Xアカウント@SORAREVO</li>
-            </NuxtLink>
-            <p>説明・抜きポイント[純粋な新社会人女性を様々なアングルで嬲り歪ませるのは最高でした。現実じゃできないことができるので少し変わった趣味をお持ちの方は必見です！]</p>
-          </div>
-        </a>
-      </div>
+      <cards />
   
       <!-- ドーナドーナのゲーム紹介カード -->
       <div class="card">
@@ -54,7 +39,7 @@
                 <li>Xアカウント:@yuzusoft</li>
               </NuxtLink>
             </ul>
-            <p>説明・抜きポイント：このゲームは</p>
+            <p>説明・抜きポイント：このゲームはストーリーがすごすぎます！私が涙と精子が一緒に出ちゃいました。</p>
           </div>
         </a>
       </div>
